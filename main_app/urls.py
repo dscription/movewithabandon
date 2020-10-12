043355584aa9e0eb3a86af42a0f2b31e852b5e1d
dscription <dscriptionplayground@gmail.com>
2020-10-12 11:27:50 -0400

    navbar setup

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns =[
-  path('', views.home, name='home')
-  # add path for Gallery View
+  path('', views.home, name='home'),
+  path('gallery', views.gallery, name='gallery'),
+  path('accounts/signup/', views.signup, name='signup')
+ 
   
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+import uuid
+import boto3
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def gallery(request):
+  return render(request, 'gallery.html')
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
