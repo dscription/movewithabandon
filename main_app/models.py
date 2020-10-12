3ea67a813e8158a75516419ccaf7e1199609d800
dscription <dscriptionplayground@gmail.com>
2020-10-12 09:46:41 -0400

    mock up video model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Video(models.Model):
+  url = models.CharField(max_length=200)
+  isPrivate = models.BooleanField(default=False)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for user_id: {self.user_id} @{self.url}"
