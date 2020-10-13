3754547b4228f6e5240afafc9960ff2fa5567660
dscription <dscriptionplayground@gmail.com>
2020-10-12 20:06:26 -0400

    add videos views to urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,12 @@ from .import views
 urlpatterns =[
   path('', views.home, name='home'),
   path('gallery', views.gallery, name='gallery'),
-  path('accounts/signup/', views.signup, name='signup')
- 
+  path('accounts/signup/', views.signup, name='signup'),
+  # videos views
+  path('videos/', views.videos_index, name='index'),
+  path('videos/create/', views.VideoCreate.as_view()),
+  path('videos/<int:pk>/update', views.VideoUpdate.as_view(),name='videos_update'),
+  path('videos/<int:pk>/update', views.VideoDelete.as_view(),name='videos_delete'),
+
   
 ]
