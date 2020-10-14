21af38f481412ab8533f054a29733997ea1194f8
dscription <dscriptionplayground@gmail.com>
2020-10-14 10:29:07 -0400

    add dateCreated field to model

--- main_app/models.py
+++ main_app/models.py
@@ -8,6 +8,7 @@ class Video(models.Model):
   url = models.CharField(max_length=200)
   isPrivate = models.BooleanField(default=False)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+  dateCreated = models.DateField(default=date.today)
 
   def __str__(self):
-    return f"Photo for user_id: {self.user_id} @{self.url}"
+    return f"Video for user_id: {self.user_id} @{self.url} private: {self.isPrivate}"
